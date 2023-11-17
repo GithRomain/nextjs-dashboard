3e2d1645731f20a23c8c38dc1eed315eb92c982e
Githromix <90053839+GithRomain@users.noreply.github.com>
2023-11-17 02:34:39 +0100

    resolve bug
    Deleting invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -71,7 +71,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Hello");
   try {
     await sql`DELETE
                   FROM invoices
